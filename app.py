4540c722d9b127f6d36f18029adec105e564eb96
Ben Zordan <cms.benzordan@gmail.com>
2023-06-08 10:49:37 +0800

    changed functionality

--- app.py
+++ app.py
@@ -1,88 +1,47 @@
 import os
-import telegram
-from telegram.ext import Updater, MessageHandler, filters
-from telegram.ext import CommandHandler
-from dictionary import get_info
 
-telegram_bot_token = "6259661929:AAG28vS_-NGCkWydsKuDdKG7J1NrNzc4Drg"
-token = telegram_bot_token
-use_context = True
+import telebot
 
-updater = Updater(token, use_context)
-dispatcher = updater.dispatcher
+from utils import get_daily_horoscope
+
+BOT_TOKEN = os.environ.get('6259661929:AAG28vS_-NGCkWydsKuDdKG7J1NrNzc4Drg')
+
+bot = telebot.TeleBot(BOT_TOKEN)
 
 
-# set up the introductory statement for the bot when the /start command is invoked
-def start(update, context):
-    chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id=chat_id, text="Hello there. Provide any English word and I will give you a bunch "
-                                                   "of information about it.")
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
 
 
-# obtain the information of the word provided and format before presenting.
-def get_word_info(update, context):
-    # get the word info
-    word_info = get_info(update.message.text)
+@bot.message_handler(commands=['horoscope'])
+def sign_handler(message):
+    text = "What's your zodiac sign?\nChoose one: *Aries*, *Taurus*, *Gemini*, *Cancer,* *Leo*, *Virgo*, *Libra*, *Scorpio*, *Sagittarius*, *Capricorn*, *Aquarius*, and *Pisces*."
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, day_handler)
 
-    # If the user provides an invalid English word, return the custom response from get_info() and exit the function
-    if word_info.__class__ is str:
-        update.message.reply_text(word_info)
-        return
 
-    # get the word the user provided
-    word = word_info['word']
+def day_handler(message):
+    sign = message.text
+    text = "What day do you want to know?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date in format YYYY-MM-DD."
+    sent_msg = bot.send_message(
+        message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(
+        sent_msg, fetch_horoscope, sign.capitalize())
 
-    # get the origin of the word
-    origin = word_info['origin']
-    meanings = '\n'
 
-    synonyms = ''
-    definition = ''
-    example = ''
-    antonyms = ''
+def fetch_horoscope(message, sign):
+    day = message.text
+    horoscope = get_daily_horoscope(sign, day)
+    data = horoscope["data"]
+    horoscope_message = f'*Horoscope:* {data["horoscope_data"]}\n*Sign:* {sign}\n*Day:* {data["date"]}'
+    bot.send_message(message.chat.id, "Here's your horoscope!")
+    bot.send_message(message.chat.id, horoscope_message, parse_mode="Markdown")
 
-    # a word may have several meanings. We'll use this counter to track each of the meanings provided from the response
-    meaning_counter = 1
 
-    for word_meaning in word_info['meanings']:
-        meanings += 'Meaning ' + str(meaning_counter) + ':\n'
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
 
-        for word_definition in word_meaning['definitions']:
-            # extract the each of the definitions of the word
-            definition = word_definition['definition']
 
-            # extract each example for the respective definition
-            if 'example' in word_definition:
-                example = word_definition['example']
-
-            # extract the collection of synonyms for the word based on the definition
-            for word_synonym in word_definition['synonyms']:
-                synonyms += word_synonym + ', '
-
-            # extract the antonyms of the word based on the definition
-            for word_antonym in word_definition['antonyms']:
-                antonyms += word_antonym + ', '
-
-        meanings += 'Definition: ' + definition + '\n\n'
-        meanings += 'Example: ' + example + '\n\n'
-        meanings += 'Synonym: ' + synonyms + '\n\n'
-        meanings += 'Antonym: ' + antonyms + '\n\n\n'
-
-        meaning_counter += 1
-
-    # format the data into a string
-    message = f"Word: {word}\n\nOrigin: {origin}\n{meanings}"
-
-    update.message.reply_text(message)
-
-# run the start function when the user invokes the /start command 
-dispatcher.add_handler(CommandHandler("start", start))
-
-# invoke the get_word_info function when the user sends a message 
-# that is not a command.
-dispatcher.add_handler(MessageHandler(filters.text, get_word_info))
-updater.start_webhook(listen="0.0.0.0",
-                      port=int(os.environ.get('PORT', 5000)),
-                      url_path=telegram_bot_token,
-                      webhook_url=  + telegram_bot_token
-                      )
+bot.infinity_polling()
